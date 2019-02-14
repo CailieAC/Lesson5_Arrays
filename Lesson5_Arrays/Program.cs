aa692506f1b378b58f11838b4904605cf3fa4ba2
Cailie Carlile <cailiecarlile@gmail.com>
2019-02-13 20:31:20 -0600

    Add project files.

---
+++ Lesson5_Arrays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson5_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
